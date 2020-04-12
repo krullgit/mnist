bbdfa2ce37a9f73251b33b6e6328f913318fdc02
Matthes Krull <matthes.krull@dfki.de>
2020-04-12 23:29:52 +0200

    test

--- code.py
+++ code.py
@@ -1,3 +1,132 @@
 import torch
 import torchvision
 import wandb
+import numpy as np
+np.set_printoptions(threshold=np.inf)
+
+
+wandb.init(project="mnist")
+n_epochs = 1
+batch_size_train = 64
+batch_size_test = 1000
+learning_rate = 0.01
+momentum = 0.5
+log_interval = 100
+
+random_seed = 1
+torch.backends.cudnn.enabled = False
+torch.manual_seed(random_seed)
+
+train_loader = torch.utils.data.DataLoader(
+  torchvision.datasets.MNIST('./files/', train=True, download=True,
+                             transform=torchvision.transforms.Compose([
+                               torchvision.transforms.ToTensor(),
+                               torchvision.transforms.Normalize(
+                                 (0.1307,), (0.3081,))
+                             ])),
+  batch_size=batch_size_train, shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(
+  torchvision.datasets.MNIST('./files/', train=False, download=True,
+                             transform=torchvision.transforms.Compose([
+                               torchvision.transforms.ToTensor(),
+                               torchvision.transforms.Normalize(
+                                 (0.1307,), (0.3081,))
+                             ])),
+  batch_size=batch_size_test, shuffle=True)
+
+examples = enumerate(test_loader)
+batch_idx, (example_data, example_targets) = next(examples)
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # (channel_input ,channel_output )
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d(p=0.7)# https://pytorch.org/docs/stable/nn.html
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+        self.counter = 0
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        #if(self.counter<=5):
+        #    print(np.asarray(x)[0])
+        x = F.dropout(x, training=self.training) # https://pytorch.org/docs/stable/nn.functional.html?highlight=dropout
+        #if(self.counter<=5):
+        #    print(np.asarray(x)[0])
+        x = self.fc2(x)
+        #self.counter = self.counter + 1
+        return F.log_softmax(x)
+    
+network = Net()
+optimizer = optim.SGD(network.parameters(), lr=learning_rate,
+                      momentum=momentum)
+
+train_losses = []
+train_counter = []
+test_losses = []
+test_counter = [i*len(train_loader.dataset) for i in range(n_epochs + 1)]
+
+
+
+def train(epoch):
+    # just puts the network in train mode, so dropout layer etc. behave different 
+    network.train()
+    
+    # 
+    for batch_idx, (data, target) in enumerate(train_loader):
+        
+        
+        optimizer.zero_grad() #https://discuss.pytorch.org/t/why-do-we-need-to-set-the-gradients-manually-to-zero-in-pytorch/4903/2
+        output = network(data) 
+        loss = F.nll_loss(output, target)
+        
+        
+        loss.backward()
+        optimizer.step()
+        wandb.log({"Loss": loss})
+        if batch_idx % log_interval == 0:
+            test()
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+            epoch, batch_idx * len(data), len(train_loader.dataset),
+            100. * batch_idx / len(train_loader), loss.item()))
+            train_losses.append(loss.item())
+            train_counter.append(
+            (batch_idx*64) + ((epoch-1)*len(train_loader.dataset)))
+            
+            torch.save(network.state_dict(), './results/model.pth')
+            torch.save(optimizer.state_dict(), './results/optimizer.pth')
+
+def test():
+    # just puts the network in eval mode, so dropout layer etc. behave different 
+    network.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            output = network(data)
+            test_loss += F.nll_loss(output, target, size_average=False).item()
+            pred = output.data.max(1, keepdim=True)[1]
+            correct += pred.eq(target.data.view_as(pred)).sum()
+    test_loss /= len(test_loader.dataset)
+    test_losses.append(test_loss)
+    print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    test_loss, correct, len(test_loader.dataset),
+    100. * correct / len(test_loader.dataset)))
+    wandb.log({"Accuracy": 100. * correct / len(test_loader.dataset)})
+    
+test()
+for epoch in range(1, n_epochs + 1):
+    train(epoch)
+    #test()
+    
